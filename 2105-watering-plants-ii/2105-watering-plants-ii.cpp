98b68fc2e61419c25f1c57619009b14d74198afa
Prashant <prashantamoli@outlook.com>
2022-10-11 13:58:01 +0530

    Time: 384 ms (21.83%), Space: 78.3 MB (67.14%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
     int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
-      int n = plants.size();
-      int a=0, b=n-1, waterA=capacityA, waterB=capacityB, refills=0;
-      
-      while(a<=b){
-        if(a == b){ // On same plant
-          if(waterA >= waterB){
-            // Alice
-            if(waterA < plants[a]){
-              waterA = capacityA;
-              refills++;
-            }
-            waterA -= plants[a];
-          }
-          else{ 
-            // Bob
-            if(waterB < plants[b]){
-              waterB = capacityB;
-              refills++;
-            }
-            waterB -= plants[b];
-          }
-          break;
-        }
-        
-        // Alice
-        if(waterA < plants[a]){
-          waterA = capacityA;
-          refills++;
-        }
-        waterA -= plants[a];
-        
-        // Bob
-        if(waterB < plants[b]){
-          waterB = capacityB;
-          refills++;
-        }
-        waterB -= plants[b];
-        
-        a++;
-        b--;
+      int i = 0, j = plants.size() - 1, canA = capacityA, canB = capacityB, res = 0;
+      while (i < j) {
+        res += (canA < plants[i]) + (canB < plants[j]);
+        canA = canA < plants[i] ? capacityA : canA;
+        canB = canB < plants[j] ? capacityB : canB;
+        canA -= plants[i++];
+        canB -= plants[j--];
       }
-      return refills;
+      return res + (i == j && max(canA, canB) < plants[i]);
+      
+//       int n = plants.size();
+//       int a=0, b=n-1, waterA=capacityA, waterB=capacityB, refills=0;
+      
+//       while(a<=b){
+//         if(a == b){ // On same plant
+//           if(waterA >= waterB){
+//             // Alice
+//             if(waterA < plants[a]){
+//               waterA = capacityA;
+//               refills++;
+//             }
+//             waterA -= plants[a];
+//           }
+//           else{ 
+//             // Bob
+//             if(waterB < plants[b]){
+//               waterB = capacityB;
+//               refills++;
+//             }
+//             waterB -= plants[b];
+//           }
+//           break;
+//         }
+        
+//         // Alice
+//         if(waterA < plants[a]){
+//           waterA = capacityA;
+//           refills++;
+//         }
+//         waterA -= plants[a];
+        
+//         // Bob
+//         if(waterB < plants[b]){
+//           waterB = capacityB;
+//           refills++;
+//         }
+//         waterB -= plants[b];
+        
+//         a++;
+//         b--;
+//       }
+//       return refills;
     }
 };
