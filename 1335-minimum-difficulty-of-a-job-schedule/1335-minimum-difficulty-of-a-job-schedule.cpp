638cfb4a3834bef0638165a3de48b0904e16aaf8
Prashant <prashantamoli@outlook.com>
2022-10-16 23:23:46 +0530

    Time: 152 ms (31.00%), Space: 8.4 MB (18.05%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,4 +1,5 @@
 class Solution {
+public:
     // TABULATION
     int difficulty(vector<int> &jobDifficulty, int n, int d){
       vector<vector<int>> dp(n + 1, vector<int> (d + 1, 1e9));
@@ -15,7 +16,6 @@ class Solution {
       }
       return dp[n][d];
     }
-
 
     // MEMOIZATION
     int difficulty(vector<int> &jobDifficulty, int start, int n, int d, vector<vector<int>> &dp){
@@ -38,11 +38,11 @@ class Solution {
       return dp[start][d] = res;
     }
   
-public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
       int n = jobDifficulty.size();
       if(d > n) return -1;
       vector<vector<int>> dp(n + 1, vector<int> (d + 1, -1));
-      return difficulty(jobDifficulty, 0, n, d, dp);
+      // return difficulty(jobDifficulty, 0, n, d, dp);
+      return difficulty(jobDifficulty, n, d);
     }
 };
