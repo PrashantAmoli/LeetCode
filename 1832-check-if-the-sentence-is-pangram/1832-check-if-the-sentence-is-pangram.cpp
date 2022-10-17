c96c883b4c413eb630178eb0277920275e4e9d58
Prashant <prashantamoli@outlook.com>
2022-10-18 00:12:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (31.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-      vector<int> v(26, 0);
+      // vector<int> v(26, 0);
+      // for(int i=0; i<sentence.size(); i++)
+      //   v[sentence[i]-'a'] = 1;
+      // return accumulate(v.begin(), v.end(), 0) == 26;
+      
+      set<int> st;
       for(int i=0; i<sentence.size(); i++)
-        v[sentence[i]-'a'] = 1;
-      return accumulate(v.begin(), v.end(), 0) == 26;
+        st.insert(sentence[i] - 'a');
+      return st.size() == 26;
     }
 };
