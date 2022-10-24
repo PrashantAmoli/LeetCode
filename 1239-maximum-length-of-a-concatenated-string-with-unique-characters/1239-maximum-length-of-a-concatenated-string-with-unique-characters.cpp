939b81e191f0fa01abbdb2a0ff86203c28b713ed
Prashant <prashantamoli@outlook.com>
2022-10-24 18:05:11 +0530

    Time: 3 ms (98.05%), Space: 8.2 MB (86.03%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -33,10 +33,47 @@ public:
       solve(i+1, s, arr, used);
     }
   
+    void solveFast(int i, string &s, vector<string>& arr, vector<int>& used){
+      if(i == arr.size()) 
+        return;
+      
+      bool unique = true;
+      int size = arr[i].size();
+      
+      // Take if unique possible
+      if(size + s.size() <= 26){
+        size = 0;
+        for(auto &c: arr[i]){
+          used[c-'a']++;
+          s += c;
+          size++;
+          if(used[c-'a'] > 1) {
+            unique = false;
+            break;
+          }
+        }
+        
+        if(unique){ // if unique take and move
+          int len = s.size();
+          ans = max(ans, len);
+          solveFast(i+1, s, arr, used);
+        }
+        
+        while(size--){ // pop arr[i].size() elements from s
+          used[s.back()-'a']--;
+          s.pop_back();
+        }
+      }
+      
+      // Leave
+      solveFast(i+1, s, arr, used);
+    }
+  
     int maxLength(vector<string>& arr) {
       vector<int> used(26, 0);
       string s = "";
-      solve(0, s, arr, used);
+      // solve(0, s, arr, used);
+      solveFast(0, s, arr, used);
       return ans;
     }
 };
