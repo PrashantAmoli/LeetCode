19070605b2f8a54f995fc70e78be1b05cc2b8eda
Prashant <prashantamoli@outlook.com>
2022-10-25 11:40:02 +0530

    Time: 22 ms (5.57%), Space: 11.3 MB (97.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -4,15 +4,25 @@ public:
       string s1 = "";
       for(auto &str: word1) s1 += str;
       
+//       string s2 = "";
+//       for(auto &str: word2) s2 += str;
+      
+//       int n1 = s1.size();
+//       if(n1 != s2.size()) return false;
+      
+//       for(int i=0; i<n1; i++)
+//         if(s1[i] != s2[i])
+//           return false;
+//       return true;
+      
       string s2 = "";
-      for(auto &str: word2) s2 += str;
+      int it=0;
       
-      int n1 = s1.size();
-      if(n1 != s2.size()) return false;
+      for(int i=0; i<word2.size(); i++)
+        for(auto ch: word2[i])
+          if(ch != s1[it++])
+            return false;
       
-      for(int i=0; i<n1; i++)
-        if(s1[i] != s2[i])
-          return false;
-      return true;
+      return (it != s1.size()) ? false : true;
     }
 };
